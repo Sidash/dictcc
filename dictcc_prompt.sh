0b1784b620bb4f5754c70f2537cb169eada27e6a
Simon Schmidt <sd.schmidt@sikos.de>
2017-05-31 15:57:42 +0200

    Add dictcc_promot() script

---
+++ dictcc_prompt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# simple script for executing dictcc continiously
+
+alias dictcc='./dictcc.py' # change this line
+
+export dict="deen"
+function dictcc_prompt() {  
+    clear;
+    while true;
+        do echo -n "Search in $dict >> ";
+        read query;
+        echo "\n\n--------------------------------------------------------------------------------"
+        clear;
+        echo "Searching in '$dict' for '$query'";
+        dictcc $dict $query;
+        echo
+    done;
+}
